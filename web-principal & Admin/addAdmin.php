807533168a3e630fa0675f73be23d66a9531b92a
Reema Shabibi <441201234@student.ksu.edu.sa>
2022-10-16 15:57:51 +0300

    create forms 2

--- web-principal & Admin/addAdmin.php
+++ web-principal & Admin/addAdmin.php
@@ -4,52 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="create.css">
+<script src="less.js" type="text/javascript"></script>
+
+    <title>إضافة إداري</title>
 </head>
+
 <body>
-    
 
-<div class="page">
-  <div class="container">
-    <div class="left">
-      <div class="login">Login</div>
-      <div class="eula">By logging in you agree to the ridiculously long terms that you didn't bother to read</div>
+<div class="center">
+  <h1>إضافة إداري</h1>
+  <form >
+
+  <div class="inputbox">
+      <input type="text" required="required">
+      <span>الاسم الأول</span>
     </div>
-    <div class="right">
-      <svg viewBox="0 0 320 300">
-        <defs>
-          <linearGradient
-                          inkscape:collect="always"
-                          id="linearGradient"
-                          x1="13"
-                          y1="193.49992"
-                          x2="307"
-                          y2="193.49992"
-                          gradientUnits="userSpaceOnUse">
-            <stop
-                  style="stop-color:#ff00ff;"
-                  offset="0"
-                  id="stop876" />
-            <stop
-                  style="stop-color:#ff0000;"
-                  offset="1"
-                  id="stop878" />
-          </linearGradient>
-        </defs>
-        <path d="m 40,120.00016 239.99984,-3.2e-4 c 0,0 24.99263,0.79932 25.00016,35.00016 0.008,34.20084 -25.00016,35 -25.00016,35 h -239.99984 c 0,-0.0205 -25,4.01348 -25,38.5 0,34.48652 25,38.5 25,38.5 h 215 c 0,0 20,-0.99604 20,-25 0,-24.00396 -20,-25 -20,-25 h -190 c 0,0 -20,1.71033 -20,25 0,24.00396 20,25 20,25 h 168.57143" />
-      </svg>
-      <div class="form">
-        <label for="email">Email</label>
-        <input type="email" id="email">
-        <label for="password">Password</label>
-        <input type="password" id="password">
-        <input type="submit" id="submit" value="Submit">
-      </div>
+
+    <div class="inputbox">
+      <input type="text" required="required">
+      <span>الاسم  الأخير</span>
     </div>
-  </div>
+
+
+    <div class="inputbox">
+      <input type="text" required="required">
+      <span>البريد الإلكتروني</span>
+    </div>
+
+
+    <div class="inputbox">
+      <input type="button" value="إضافة">
+    </div>
+  </form>
 </div>
-
-
 
 <!-- randomly generated pass -->
 <?php
@@ -57,6 +45,10 @@ $comb = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 $shfl = str_shuffle($comb);
 $pwd = substr($shfl,0,8);
 ?>
+
+<!-- sending email -->
+
+
 </body>
 </html>
 
