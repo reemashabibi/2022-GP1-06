bed85bf60c57a40277dabc445dd36156c4fbe979
GhadeerW <441925557@student.ksu.edu.sa>
2022-10-16 17:17:43 +0300

    addStudent js and php , admin.js

--- web-principal & Admin/addStudent.php
+++ web-principal & Admin/addStudent.php
@@ -4,56 +4,61 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="create.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/firebase/7.14.1-0/firebase.js"></script>
-    <script type="module" src="addStudent.js"></script>
-    <script src="create.js"></script>
+<script type="module" src="addStudent.js" type="text/javascript"></script>
+
+    <title>إضافة طالب</title>
 </head>
+
 <body>
 
+<div class="center">
+  <h1>إضافة طالب</h1>
+  <form id="add">
 
-<div class="page">
-  <div class="container">
-    <div class="left">
-      <div class="login">Login</div>
-      <div class="eula">By logging in you agree to the ridiculously long terms that you didn't bother to read</div>
+  <div class="inputbox">
+      <input id="Fname" type="text" required="required">
+      <span>الاسم الأول</span>
     </div>
-    <div class="right">
-      <svg viewBox="0 0 320 300">
-        <defs>
-          <linearGradient
-                          inkscape:collect="always"
-                          id="linearGradient"
-                          x1="13"
-                          y1="193.49992"
-                          x2="307"
-                          y2="193.49992"
-                          gradientUnits="userSpaceOnUse">
-            <stop
-                  style="stop-color:#ff00ff;"
-                  offset="0"
-                  id="stop876" />
-            <stop
-                  style="stop-color:#ff0000;"
-                  offset="1"
-                  id="stop878" />
-          </linearGradient>
-        </defs>
-        <path d="m 40,120.00016 239.99984,-3.2e-4 c 0,0 24.99263,0.79932 25.00016,35.00016 0.008,34.20084 -25.00016,35 -25.00016,35 h -239.99984 c 0,-0.0205 -25,4.01348 -25,38.5 0,34.48652 25,38.5 25,38.5 h 215 c 0,0 20,-0.99604 20,-25 0,-24.00396 -20,-25 -20,-25 h -190 c 0,0 -20,1.71033 -20,25 0,24.00396 20,25 20,25 h 168.57143" />
-      </svg>
-      <div class="form">
-        <label for="email">Email</label>
-        <input type="email" id="email">
-        <label for="password">Password</label>
-        <input type="password" id="password">
-        <input type="submit" id="submit" value="Submit">
-      </div>
+
+    <div class="inputbox">
+      <input id="Lname" type="text" required="required">
+      <span>الاسم  الأخير</span>
     </div>
-  </div>
+
+    <div class="inputbox" ">
+      <select style="  margin-top: 15px; padding: 10px; background:#ccc; border: 2px solid #000; width:40%; height: 30px; text-align:center; " name="class" id="class" required="required">
+            <option value="non"></option>
+        </select>
+      <span style="border: 2px solid #000; width:40%;  text-align:center; border-radius: 10px; font-size: 1.2em; position: absolute; ">الفصل</span>
+    </div>
+
+    <div class="inputbox">
+      <input type="text" required="required">
+      <span>البريد الإلكتروني لولي الأمر</span>
+      <label for="" ><br><br><br><a href="#" onclick="openForm()" class="open-button" style=" display: flex; justify-content: right;">ولي أمر جديد</a></label>
+    </div>
+
+    
+    <div class="inputbox">
+        <button>إضافة</button>
+      <input type="button" value="إضافة">
+    </div>
+  </form>
 </div>
 
-</body>
+<!-- randomly generated pass -->
+<?php
+$comb = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+$shfl = str_shuffle($comb);
+$pwd = substr($shfl,0,8);
+?>
 
+<!-- sending email -->
+
+
+</body>
 </html>
+
+
+
