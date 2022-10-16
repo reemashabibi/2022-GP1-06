b6e2b09d7fadf7899818fd3b4fccceec0bb194ae
Reema Shabibi <441201234@student.ksu.edu.sa>
2022-10-16 17:31:04 +0300

    create validate

--- web-principal & Admin/addAdmin.php
+++ web-principal & Admin/addAdmin.php
@@ -5,48 +5,73 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="create.css">
-<script src="less.js" type="text/javascript"></script>
-
     <title>إضافة إداري</title>
+    <!-- add database connection + session -->
 </head>
 
 <body>
-
 <div class="center">
   <h1>إضافة إداري</h1>
-  <form >
+
+  <form action="addAdminValid.php" method="POST" onsubmit="return validate();">
 
   <div class="inputbox">
-      <input type="text" required="required">
+      <input type="text" required="required" name ="firstName" id="firstName">
       <span>الاسم الأول</span>
     </div>
 
     <div class="inputbox">
-      <input type="text" required="required">
+      <input type="text" required="required" name ="lastName" id="lastName">
       <span>الاسم  الأخير</span>
     </div>
 
 
     <div class="inputbox">
-      <input type="text" required="required">
+      <input type="email" required="required" name ="email" id="email">
       <span>البريد الإلكتروني</span>
     </div>
 
 
     <div class="inputbox">
-      <input type="button" value="إضافة">
+      <input type="button" value="إضافة" name="rgs_btn" >
     </div>
   </form>
 </div>
 
-<!-- randomly generated pass -->
-<?php
-$comb = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-$shfl = str_shuffle($comb);
-$pwd = substr($shfl,0,8);
-?>
 
-<!-- sending email -->
+<!-- validate -->
+<script type="text/javascript">
+function validate() {
+ var fname = document.getElementById( "firstName" );
+ var letters = /^[A-Za-z]+$/;
+ if( !fname.value.match(letters) )
+ {
+  alert('first name must have alphabet characters only');
+  document.addAdmin.firstName.focus();
+  return false;
+ }
+
+ var lname = document.getElementById( "lastName" );
+ if( !lname.value.match(letters) )
+ {
+  alert('last name must have alphabet characters only');
+  document.addAdmin.lastName.focus();
+  return false;
+ }
+
+ var email = document.getElementById( "email" );
+ var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+ if( !email.value.match(mailformat)){
+  alert("You have entered an invalid email address!");
+  document.addAdmin.email.focus();
+  return false;
+ }
+
+ else {
+   return true;
+  }
+}
+</script>
 
 
 </body>
