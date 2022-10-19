6869ed4c14f5569ba73fc597c5e17b7928de8639
GhadeerW <441925557@student.ksu.edu.sa>
2022-10-20 00:11:44 +0300

    parent auth-1

--- web-principal & Admin/addStudent.js
+++ web-principal & Admin/addStudent.js
@@ -1,20 +1,15 @@
 
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import {
+  getAuth, createUserWithEmailAndPassword, onAuthStateChanged, sendEmailVerification, updatePassword, sendPasswordResetEmail, fetchSignInMethodsForEmail
+} from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
-import { collection, getDocs, addDoc, Timestamp, deleteDoc } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
+import { collection, getDocs, addDoc, Timestamp, setDoc } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
 import { query, orderBy, limit, where, onSnapshot } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
-import { get, ref } from "https://www.gstatic.com/firebasejs/9.12.1//firebase-database.js"
-import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
-// import firebase from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
-//import "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { doc } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAk1XvudFS302cnbhPpnIka94st5nA23ZE",
   authDomain: "halaqa-89b43.firebaseapp.com",
@@ -24,14 +19,15 @@ const firebaseConfig = {
   appId: "1:969971486820:web:40cc0abf19a909cc470f71",
   measurementId: "G-PCYTHJF1SD"
 };
-// Initialize Firebase
-//firebase.initializeApp(firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { app, db, collection, getDocs, Timestamp, addDoc };
 export { query, orderBy, limit, where, onSnapshot };
 const analytics = getAnalytics(app);
+
+
 
 
 //add class to drop-down menu
@@ -59,163 +55,289 @@ const colRefStudent = collection(db, "Student");
 const colRefParent = collection(db, "Parent");
 const selectedClass = document.getElementById("classes");
 const addStudentForm = document.querySelector('.add')
-var notValidated= false ; 
+var notValidated = false;
+var filledCorrectly = false;
 
 
 
 addStudentForm.addEventListener('submit', async (e) => {
-notValidated= false ; 
-var fname = document.getElementById( "Fname" );
-var letters = /^[A-Za-z]+$/;
-if( !fname.value.match(letters) )
-{
- alert('.يلزم ان يتكون الاسم الأول للطالب من احرف فقط');
- fname.focus();
- notValidated = true;
-}
+  notValidated = false;
+  var fname = document.getElementById("Fname");
+  var letters = /^[A-Za-z]+$/;
+  if (!fname.value.match(letters)) {
+    alert('.يلزم ان يتكون الاسم الأول للطالب من احرف فقط');
+    fname.focus();
+    notValidated = true;
+  }
 
-var Lname = document.getElementById( "Lname" );
-if( !Lname.value.match(letters) )
-{
- alert('.يلزم ان يتكون الاسم الأخير للطالب من احرف فقط');
- Lname.focus();
- notValidated = true;
-}
+  var Lname = document.getElementById("Lname");
+  if (!Lname.value.match(letters)) {
+    alert('.يلزم ان يتكون الاسم الأخير للطالب من احرف فقط');
+    Lname.focus();
+    notValidated = true;
+  }
 
-var selectedClass = document.getElementById("classes");
-var selectedClassIn = selectedClass[selectedClass.selectedIndex].value;
-if(selectedClassIn == "non"){
- alert('.أرجو اختيار الفصل');
- document.querySelector('.add').non.focus();
- notValidated = true;}
-       
+  var selectedClass = document.getElementById("classes");
+  var selectedClassIn = selectedClass[selectedClass.selectedIndex].value;
+  if (selectedClassIn == "non") {
+    alert('.أرجو اختيار الفصل');
+    document.querySelector('.add').non.focus();
+    notValidated = true;
+  }
 
 
 
- var phoneNo =document.getElementById("phone");
-          var phoneno = /^\d{10}$/;
-          if((!phoneNo.value.match(phoneno)))
-                {
-                 alert('يلزم ان يتكون رقم الهاتف من ١٠ ارقام فقط');
-                phoneNo.focus();
-                notValidated = true;
-                }
-       
 
-         
-         var FnameParent = document.getElementById( "FnameParent" );
-         var letters = /^[A-Za-z]+$/;
-         if( !FnameParent.value.match(letters) )
-         {
-           alert('.يلزم ان يتكون الاسم الأول لولي الأمر من احرف فقط');
-          FnameParent.focus();
-          notValidated = true;
-         }
-     
-         var LnameParent = document.getElementById( "LnameParent" );
-         if( !LnameParent.value.match(letters) )
-         {
-           alert('.يلزم ان يتكون الاسم الأخير لولي الأمر من احرف فقط');
-          LnameParent.focus();
-          notValidated = true;
-         }
+  var phoneNo = document.getElementById("phone");
+  var phoneno = /^\d{10}$/;
+  if ((!phoneNo.value.match(phoneno))) {
+    alert('يلزم ان يتكون رقم الهاتف من ١٠ ارقام فقط');
+    phoneNo.focus();
+    notValidated = true;
+  }
 
-         var emailP = document.getElementById( "emailP" );
-         var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-         if( !emailP.value.match(mailformat)){
-           alert('.ارجوا ادخال بريد الكتروني صالح');
-          emailP.focus();
-          notValidated = true;}
 
-          
-          if(notValidated)
-          e.preventDefault();
 
-         });
+  var FnameParent = document.getElementById("FnameParent");
+  var letters = /^[A-Za-z]+$/;
+  if (!FnameParent.value.match(letters)) {
+    alert('.يلزم ان يتكون الاسم الأول لولي الأمر من احرف فقط');
+    FnameParent.focus();
+    notValidated = true;
+  }
+
+  var LnameParent = document.getElementById("LnameParent");
+  if (!LnameParent.value.match(letters)) {
+    alert('.يلزم ان يتكون الاسم الأخير لولي الأمر من احرف فقط');
+    LnameParent.focus();
+    notValidated = true;
+  }
+
+  var emailP = document.getElementById("emailP");
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!emailP.value.match(mailformat)) {
+    alert('.ارجوا ادخال بريد الكتروني صالح');
+    emailP.focus();
+    notValidated = true;
+  }
+
+
+  if (notValidated)
+    e.preventDefault();
+
+});
 
 addStudentForm.addEventListener('submit', async (e) => {
 
   e.preventDefault();
-  
-if(!notValidated){
 
-  ////////////////////////
-  
-  const phoneNumber = parseInt(addStudentForm.phone.value);
- const q = query(collection(db, "Parent"), where("PhoneNumber", "==", phoneNumber));
-  const querySnapshot = await getDocs(q);
-  var parentId = "null";
-  var docRef = "null";
-  var docRefClass ="null";
-  if(!querySnapshot.empty){
-  querySnapshot.forEach((doc) => {
-    if (!doc.empty)
-      parentId = doc.id;
+  if (!notValidated) {
 
-  else {
-    alert("ولي الأمر غير مسجل، الرجاء اكمال البيانات");
-    addStudentForm.reset()
-  }
-})
-}
+    ////////////////////////
 
-  docRef = doc(db, "Parent", parentId);
-  docRefClass = doc(db, "Class", selectedClass[selectedClass.selectedIndex].id);
- addDoc(colRefStudent, {
-   FirstName: addStudentForm.Fname.value,
-   LastName: addStudentForm.Lname.value,
-   ClassID: docRefClass,
-   ParentID: docRef,
- })
-   .then(() => {
-    addStudentForm.reset()
-    const phonenumber = parseInt(addStudentForm.phone.value);
-    if(parentId == "null"){
-     addDoc(colRefParent, {
-       FirstName: addStudentForm.FnameParent.value,
-       LastName: addStudentForm.LnameParent.value,
-       Email: addStudentForm.emailP.value,
-       Password: addStudentForm.password.value,
-       PhoneNumber:  phonenumber,
-     }).then(() => {
-       addStudentForm.reset()
-     })
+    const phoneNumber = parseInt(addStudentForm.phone.value);
+    const q = query(collection(db, "Parent"), where("PhoneNumber", "==", phoneNumber));
+    const querySnapshot = await getDocs(q);
+    var parentId = "null";
+    var docRef = "null";
+    var docRefClass = "null";
+    if (!querySnapshot.empty) {
+      querySnapshot.forEach((doc) => {
+        if (!doc.empty)
+          parentId = doc.id;
+
+        else {
+          alert("ولي الأمر غير مسجل، الرجاء اكمال البيانات");
+        }
+      })
+
+      docRef = doc(db, "Parent", parentId);
+      docRefClass = doc(db, "Class", selectedClass[selectedClass.selectedIndex].id);
+      addDoc(colRefStudent, {
+        FirstName: addStudentForm.Fname.value,
+        LastName: addStudentForm.Lname.value,
+        ClassID: docRefClass,
+        ParentID: docRef,
+      })
+        .then(() => {
+          addStudentForm.reset()
+        });
     }
+    if (parentId == "null") {
+      alert("triggerd");
+      const registerFname = document.getElementById("FnameParent").value;
+      const registerlname = document.getElementById("LnameParent").value;
+      const registerEmail = document.getElementById("emailP").value;
+      const registerPhone = document.getElementById("phone").value;
+      const registerPass = pass();
+      const phoneNum = parseInt(registerPhone);
+      createUserWithEmailAndPassword(auth, registerEmail, registerPass)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
 
+          //send an email to reset password
+          sendPasswordResetEmail(auth, registerEmail).then(() => {
+            // EmailSent
+            // alert(registerEmail + " -- " + auth);
+            alert("reset");
+          })
+          const res = doc(db, "Parent", user.uid)
+
+          //add to the document
+          setDoc(doc(db, "Parent", user.uid), {
+            Email: registerEmail,
+            FirstName: registerFname,
+            LastName: registerlname,
+            password: "",
+            PhoneNumber: phoneNum,
+            //schoolID?
+          }).then(() => {
+            docRef = doc(db, "Parent", res.id);
+            docRefClass = doc(db, "Class", selectedClass[selectedClass.selectedIndex].id);
+            addDoc(colRefStudent, {
+              FirstName: addStudentForm.Fname.value,
+              LastName: addStudentForm.Lname.value,
+              ClassID: docRefClass,
+              ParentID: docRef,
+            });
+            addStudentForm.reset();
+          });
+
+          alert("تم");
+        }).catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // alert("البريد الالكتروني مستخدم من قبل");
+          alert(errorMessage);
+          addStudentForm.reset();
+        });
+      // addStudentForm.reset();
+
+    }//end if
     alert("تمت اضافة الطالب بنجاح")
-   })
-}
+
+  }
 
 
 });
 
-$( ".phone" ).change(async function() {
- 
+$(".phone").change(async function () {
+
   var phoneNumber = parseInt(addStudentForm.phone.value);
 
   var q = query(collection(db, "Parent"), where("PhoneNumber", "==", phoneNumber));
   var querySnapshot = await getDocs(q);
   var parentId = "null";
-  if(!querySnapshot.empty){
-  querySnapshot.forEach((doc) => {
-    if (!doc.empty)
-    {
-      parentId = doc.id;
-      
-    $("#FnameParent").val(doc.data().FirstName);
-    $("#LnameParent").val(doc.data().LastName);
-    $("#emailP").val(doc.data().Email);
-    $("#password").val(doc.data().Password);
-  }  
-  })}else {
+  if (!querySnapshot.empty) {
+    querySnapshot.forEach((doc) => {
+      if (!doc.empty) {
+        parentId = doc.id;
+
+        $("#FnameParent").val(doc.data().FirstName);
+        $("#LnameParent").val(doc.data().LastName);
+        $("#emailP").val(doc.data().Email);
+        $("#password").val(doc.data().Password);
+      }
+    })
+  } else {
     alert("ولي الأمر غير مسجل، الرجاء اكمال البيانات");
     $("#FnameParent").val("");
     $("#LnameParent").val("");
     $("#emailP").val("");
-    $("#password").val("");}
+    $("#password").val("");
+  }
 
 
 
 });
 
 
+//////////////////////parent auth///////////////////////////
+
+const colRef = collection(db, 'Parent');
+const auth = getAuth(app);
+
+
+//get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let Parent = []
+    snapshot.docs.forEach((doc) => {
+      Parent.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(Parent)
+  })
+  .catch(err => {
+    console.log(err.mssage)
+  });
+
+//randomly generated pass
+function pass() {
+  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  var string_length = 8;
+  var pass = '';
+  for (var i = 0; i < string_length; i++) {
+    var rnum = Math.floor(Math.random() * chars.length);
+    pass += chars.substring(rnum, rnum + 1);
+  }
+  return pass;
+}
+
+
+
+
+/*
+// addStudentForm 
+addStudentForm.addEventListener('submit',  async (e) => {
+  if(filledCorrectly){
+    e.preventDefault()
+    alert("triggerd");
+    const registerFname = document.getElementById("FnameParent").value;
+    const registerlname = document.getElementById("LnameParent").value;
+    const registerEmail = document.getElementById("emailP").value;
+    const registerPhone = document.getElementById("phone").value;
+    const registerPass =  pass();
+    createUserWithEmailAndPassword(auth, registerEmail, registerPass)
+    .then(  (userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        
+       //send an email to reset password
+       sendPasswordResetEmail(auth,registerEmail).then(() => {
+        // EmailSent
+       // alert(registerEmail + " -- " + auth);
+        alert("reset");
+      })
+
+      //add to the document
+      setDoc(doc(db, "Parent", user.uid), {
+        Email: registerEmail,
+        FirstName: registerFname,
+        LastName: registerlname, 
+        password: "",
+        phone:registerPhone,
+        //schoolID?
+        schoolID: "/School/"+22,
+      });
+
+     alert("تم بنجاح");
+       
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+       // alert("البريد الالكتروني مستخدم من قبل");
+        alert(errorMessage);
+        addStudentForm.reset();
+      });
+      addStudentForm.reset();
+
+
+
+  }//end if
+  else{
+    alert("not filled yet.")
+  }
+
+});//end addStudentForm.addEventListener */
