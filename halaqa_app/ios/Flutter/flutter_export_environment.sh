8780ca3a92c9db58522558fda62b805510c3eff6
Reema Shabibi <441201234@student.ksu.edu.sa>
2022-12-28 19:31:04 +0300

    bug fix (files)

--- halaqa_app/ios/Flutter/flutter_export_environment.sh
+++ halaqa_app/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-<<<<<<< HEAD
 export "FLUTTER_ROOT=/Users/reemafahad/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/reemafahad/Desktop/2022-GP1-06/halaqa_app"
-=======
-export "FLUTTER_ROOT=/Users/maca7/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/maca7/Desktop/2022-GP1-06/halaqa_app"
->>>>>>> e466e870116e4ce04d4319765acfc3c5f98159f9
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=/Users/reemafahad/Desktop/2022-GP1-06/halaqa_app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
