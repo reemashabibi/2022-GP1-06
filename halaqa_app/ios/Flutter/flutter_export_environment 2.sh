c86c34abb9c48356607cf89320546bbfa0c62d98
NjoudAlfahad <441201342@student.ksu.edu.sa>
2023-03-31 21:18:33 +0300

    j

--- halaqa_app/ios/Flutter/flutter_export_environment 2.sh
+++ halaqa_app/ios/Flutter/flutter_export_environment 2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/reemafahad/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/reemafahad/Desktop/2022-GP1-06/halaqa_app"
+export "FLUTTER_ROOT=/Users/njoudalfahad/Downloads/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/njoudalfahad/Downloads/2022-GP1-06/halaqa_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- halaqa_app/ios/Flutter/flutter_export_environment.sh
+++ halaqa_app/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/reemafahad/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/reemafahad/Desktop/2022-GP1-06/halaqa_app"
+export "FLUTTER_ROOT=/Users/njoudalfahadafahad/Downloads/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/njoudalfahad/Downloads/2022-GP1-06/halaqa_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-<<<<<<< HEAD
-export "FLUTTER_TARGET=/Users/reemafahad/Desktop/2022-GP1-06/halaqa_app/lib/main.dart"
-=======
 export "FLUTTER_TARGET=/Users/njoudalfahad/Downloads/2022-GP1-06/halaqa_app/lib/main.dart"
->>>>>>> aecfca15 (final pickup)
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
